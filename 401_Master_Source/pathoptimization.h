cc692a64a599565e5e21bf52557fff17c09fbaaa
Erick <estirtz@gmail.com>
2018-10-24 01:38:36 -0700

    Revert "Fixed angleTo"
    This reverts commit 6f3feeae6c9556548ac497437d09f43a6dcca2d6.

--- 401_Master_Source/pathoptimization.h
+++ 401_Master_Source/pathoptimization.h
@@ -39,6 +39,7 @@ void distanceToTarget(RobotPose us, BallPosition* target)
 
 void angleTo(RobotPose us, BallPosition* target)
 {
+<<<<<<< HEAD
   double ourAngle = (double)(us.theta)/1000.0;
   double usX = tan(ourAngle+(PI));
   double usY = 1;
@@ -46,6 +47,12 @@ void angleTo(RobotPose us, BallPosition* target)
   double targetX = target->x - usX;
   double targetY = target->y - usY;
   
+=======
+
+  int16_t usX = tan(((us.theta)/1000));
+  int16_t usY = 1;
+  double usMag = sqrt(usX^2+1);
+>>>>>>> parent of 6f3feea... Fixed angleTo
   //for(int i = 0; i < NUM_BALLS ; i++)
   //{
     //unit vectors towards the ball
@@ -53,9 +60,13 @@ void angleTo(RobotPose us, BallPosition* target)
   //double unitY = target.y/target.distanceTo;
 
     
+<<<<<<< HEAD
   //target->angleTo = acos((dotProd(usX,usY,targetX,targetY))/(target->distanceTo * usMag));
  
   target->angleTo = (atan2((double)(target->x - us.x), (double)(target->y - us.y)) + (double)us.theta/1000)*(180/PI);
+=======
+  target->angleTo = acos((dotProd(usX,usY,target->x,target->y))/(target->distanceTo * usMag));
+>>>>>>> parent of 6f3feea... Fixed angleTo
   //}
 }
 
